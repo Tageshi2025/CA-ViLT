055db92fd4ec1b068fe3be3c903a3be94911cf67
Tageshi <fuzongjing@foxmail.com>
2025-06-16 10:02:24 +0800

    feat: print logs n plots

--- model/losses.py
+++ model/losses.py
@@ -1,3 +1,11 @@
+'''
+Author: Tageshi fuzongjing@foxmail.com
+Date: 2025-06-12 15:21:21
+LastEditors: Tageshi fuzongjing@foxmail.com
+LastEditTime: 2025-06-13 10:56:19
+FilePath: /CA-ViLT/model/losses.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import torch
 import torch.nn.functional as F
 
